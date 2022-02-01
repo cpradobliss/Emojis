f26c412f6667565c1a730a48c90f95c656c0bc47
Charles Prado <cprado@blissapplications.com>
2022-02-01 16:15:18 +0000

    Initial Commit

---
+++ EmojisApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojisApp
+//
+//  Created by Charles Prado on 01/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
